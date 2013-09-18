280a80419a8740d1cc4d851d4dc47af9db52c15c
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-18 20:45:51 -0300

    updated pkg

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = function (grunt) {
-    grunt.loadTasks('src/tasks');
 
     grunt.initConfig({
         jshint: {
@@ -14,4 +13,7 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.registerTask('default', ['jshint']);
+
+    grunt.loadTasks('./src/tasks');
+
 };
