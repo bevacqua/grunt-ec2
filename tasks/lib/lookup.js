96a967c88d9b10770cd145fc467d6945dafedd9d
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-18 23:17:44 -0300

    new file

---
+++ tasks/lib/lookup.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _ = require('lodash');
+var grunt = require('grunt');
+var chalk = require('chalk');
+var exec = require('./exec.js');
+
+module.exports = function(name, done){
+
+    grunt.log.writeln('Looking up EC2 instances named %s...', chalk.cyan(name));
+
+    exec('aws ec2 describe-instances --filters Name=tag:Name,Values=%s', [name], parse, true);
+
+    function parse(stdout){
+        var result = JSON.parse(stdout);
+        var instances = _.pluck(result.Reservations, 'Instances');
+        var flat = _.flatten(instances);
+        if (flat.length > 1) {
+            grunt.fatal('Found more than one instance tagged ' + chalk.magenta(name));
+        } else if (flat.length === 0) {
+            grunt.fatal('There are no instances tagged ' + chalk.magenta(name));
+        }
+
+        done(flat[0]);
+    }
+};
