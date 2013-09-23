519171cf01e467352069fdc7f224bb6cb0d27731
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-23 16:05:42 -0300

    pass arguments somehow else

--- tasks/lib/parse.js
+++ tasks/lib/parse.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var _ = require('lodash');
+var util = require('util');
 
 module.exports = {
     env: function (dictionary) {
         var pairs = [];
 
         _.forIn(dictionary, function (value, key) {
-            pairs.push(key + '=' + JSON.stringify(value));
+            var json = JSON.stringify(value);
+            var pair = util.format('--%s=%s', key, json);
+            pairs.push(pair);
         });
 
         return pairs.join(' ');
