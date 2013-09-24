f2700603ba5676f07709a97abb47417f3979b3e7
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-09-24 18:42:38 -0300

    fixed nginx tasks

--- tasks/ec2_nginx.js
+++ tasks/ec2_nginx.js
@@ -6,9 +6,9 @@ module.exports = function(grunt){
     var sshTask = require('./lib/sshTask.js')(grunt);
     var tasks = [
         { name: 'ec2_nginx_reload', command: 'sudo nginx -s reload' },
-        { name: 'ec2_nginx_restart', command: 'sudo nginx -s restart' },
-        { name: 'ec2_nginx_start', command: 'sudo nginx -s start' },
-        { name: 'ec2_nginx_stop', command: 'sudo nginx -s stop' }
+        { name: 'ec2_nginx_restart', command: 'sudo service nginx restart' },
+        { name: 'ec2_nginx_start', command: 'sudo service nginx start' },
+        { name: 'ec2_nginx_stop', command: 'sudo service nginx stop' }
     ];
 
     _.each(tasks, sshTask.register);
